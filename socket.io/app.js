bc6836a002ecc3b204b4a4a47bf2974d00fc1a69
LegendaryZealot <875626439@qq.com>
2019-04-26 11:07:44 +0800

    sock io demo modify

--- socket.io/app.js
+++ socket.io/app.js
@@ -3,23 +3,15 @@ var express = require("express");
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(80);
+var argv=process.argv.slice(2)
+server.listen(argv[0]);
 
 app.use(express.static(__dirname + '/staticRes'));
 
 io.on('connection', function (socket) {
   console.log('websocket has connected')
-  socket.emit('message', { hello: '欢迎链接' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-    socket.emit('message', { hello: '发送成功' });
-  });
+  socket.emit('news', { hello: '欢迎链接' });
   socket.on('say', function (data) {
-    console.log(data);
-    if (data.my === '走，一起吃饭吧') {
-      io.sockets.emit('eating', { hello: '果断走起呀！' });
-      return
-    }
-    io.sockets.emit('news', { hello: data.my });
+    io.emit('news', { hello: data.my });
   });
 });
